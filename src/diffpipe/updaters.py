6db72f2c808fefee68d31d246ff8638eccb66a2d
Patrick Wells <patrick.wells.95@gmail.com>
2026-02-24 14:38:58 -0600

    Fix updater

--- src/diffpipe/updaters.py
+++ src/diffpipe/updaters.py
@@ -4,13 +4,15 @@ import numpy as np
 def local_index_updater(sources, full_map):
     running_sum = 0
     output_data = []
+    destination_map = np.argsort(full_map)
     for source in sources:
         data = source[:]
-        data = full_map[data + running_sum]
+        data = destination_map[data + running_sum]
         output_data.append(data)
         running_sum += len(data)
 
-    return np.concat(output_data)
+    output = np.concat(output_data)
+    return output
 
 
 UPDATERS = {"top_host_idx": local_index_updater}
